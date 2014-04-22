8e27fc97a8262e90095ebc86b74a8fbaaf29241c
Afroz hussain <hussainaphroj@gmail.com>
2014-04-22 19:36:26 +0530

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 FinalApp::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
   match '/signup',    to: 'users#new',    via: 'get'
   root  'static_pages#home'
   match '/signin',  to: 'sessions#new',         via: 'get'
